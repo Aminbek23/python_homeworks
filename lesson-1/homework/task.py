2b4062e250e3fa648db1748838f02338113c118a
Aminbek23 <aminbekqudratov23@gmail.com>
2025-10-16 15:21:02 +0500

    Create task.py

---
+++ lesson-1/homework/task.py
@@ -0,0 +1,22 @@
+#task 1
+a=int(input("kavdratni tomonini kiriting:"))
+print("peremetri:",a*4)
+print("yuasi:",a**2)
+
+#task 2
+d=int(input("aylana diametrini kiriting:"))
+
+print("aylananing uzunligi:",3.14159*d)
+
+#task 3
+a=int(input("a sonni kiriting:"))
+b=int(input("b sonni kititing:"))
+print("o'rtacha qiymat:",(a+b)/2)
+
+#task 4
+a=int(input("a sonni kiriting:"))
+b=int(input("b sonni kititing:"))
+print("yig‘indisi:",a+b)
+print("ko'paytmasi:", a*b)
+print("a sonini  kvadrati:",a**2)
+print("b sonini kvadrtai:",b**2)
